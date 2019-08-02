d52370bca057a639d9488f8dc3660abd82438355
shita1112 <shibata@MBP.local>
2019-08-02 16:26:15 +0900

    letter_opnerセットアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :cats
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
