a63485cca8d670aed4bd6766b26a9ad041b26b9e
shita1112 <shibata@MBP.local>
2019-08-04 21:50:40 +0900

    assetsをCloudFrontから配信

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,9 +29,6 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
@@ -93,4 +90,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "hhh" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+
+  config.action_controller.asset_host = Rails.application.credentials.asset_host
 end
